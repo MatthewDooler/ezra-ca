0b73e1a91dcef5929c9c942c03a74ea33bccfe7b
MatthewDooler <dooler.matthew@gmail.com>
2015-05-12 13:59:26 +0100

    Improved issue cert script

--- issue-cert-from-root.sh
+++ issue-cert-from-root.sh
@@ -4,8 +4,16 @@ if [ -z ${1+x} ]; then
 else
 	echo "Issuing certificate for '$1'";
 	hostname=$1
+	echo "Generating RSA key pair"
 	openssl genrsa -out private/$hostname.key 2048
 	openssl rsa -in private/$hostname.key -outform PEM -pubout -out $hostname.pub
-	openssl req -new -sha256 -key private/$hostname.key -out $hostname.csr
-	openssl ca -config conf/ezra-root-ca.conf -in $hostname.csr -out $hostname.crt -extensions server_ext
+	echo "Generating CSR"
+	openssl req -config conf/cert.conf -new -sha256 -key private/$hostname.key -out $hostname.csr
+	echo "Signing certificate"
+	openssl ca -config conf/ezra.conf -in $hostname.csr -out $hostname.crt -extensions server_ext
+	rm -rf $hostname.csr
+	echo "Exporting"
+	openssl pkcs12 -export -inkey private/$hostname.key  -in $hostname.crt -name $hostname -out
+	openssl pkcs12 -export -out $hostname.pfx -inkey private/$hostname.key -in $hostname.crt -certfile root-ca.crt
+
 fi
