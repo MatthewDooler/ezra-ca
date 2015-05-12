dc11ae1896f5f1afb72048adb81ae18c1e8a5b38
MatthewDooler <dooler.matthew@gmail.com>
2015-05-12 14:12:15 +0100

    Issue cert script working

--- issue-cert-from-root.sh
+++ issue-cert-from-root.sh
@@ -8,12 +8,12 @@ else
 	openssl genrsa -out private/$hostname.key 2048
 	openssl rsa -in private/$hostname.key -outform PEM -pubout -out $hostname.pub
 	echo "Generating CSR"
-	openssl req -config conf/cert.conf -new -sha256 -key private/$hostname.key -out $hostname.csr
+	openssl req -new -sha256 -key private/$hostname.key -out $hostname.csr
 	echo "Signing certificate"
 	openssl ca -config conf/ezra.conf -in $hostname.csr -out $hostname.crt -extensions server_ext
 	rm -rf $hostname.csr
 	echo "Exporting"
 	openssl pkcs12 -export -inkey private/$hostname.key  -in $hostname.crt -name $hostname -out
-	openssl pkcs12 -export -out $hostname.pfx -inkey private/$hostname.key -in $hostname.crt -certfile root-ca.crt
+	openssl pkcs12 -export -out $hostname.pfx -inkey private/$hostname.key -in $hostname.crt -certfile ezra.crt
 
 fi
